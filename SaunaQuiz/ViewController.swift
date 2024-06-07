348f874c1192c370a6893028e8827b4499af516c
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2024-06-07 11:52:18 +0900

    Initial Commit

---
+++ SaunaQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SaunaQuiz
+//
+//  Created by 菅原大輝 on 2024/06/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
